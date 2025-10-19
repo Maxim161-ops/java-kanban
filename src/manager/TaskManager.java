b76e83a3ef64bd71d7f1aa8f6642dd7ce71a54ca
Maxim161-ops <ddar2508-221B@yandex.ru>
2025-10-19 15:49:11 +0300

    Добавил пустые строки между всеми методами TaskManager.java

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -3,31 +3,44 @@ package manager;
 import model.Task;
 import model.Epic;
 import model.Subtask;
+
 import java.util.List;
 
 public interface TaskManager {
     int addTask(Task task);
+
     int addEpic(Epic epic);
+
     int addSubtask(Subtask subtask);
 
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<Subtask> getAllSubtasks();
 
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Subtask getSubtask(int id);
 
     boolean updateTask(Task task);
+
     boolean updateEpic(Epic epic);
+
     boolean updateSubtask(Subtask subtask);
 
     void deleteAllTasks();
+
     void deleteAllEpics();
+
     void deleteAllSubtasks();
 
     boolean deleteTaskById(int id);
+
     boolean deleteEpicById(int id);
+
     boolean deleteSubtaskById(int id);
 
     List<Subtask> getEpicSubtasks(int epicId);
