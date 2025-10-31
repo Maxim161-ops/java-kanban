6cc0690487042ceb257b702a662df1e53be6fe28
Maxim161-ops <ddar2508-221B@yandex.ru>
2025-10-31 16:49:08 +0300

    fix: исправлено восстановление задач из файла, чтобы проходили тесты

--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -14,7 +14,7 @@ public class InMemoryTaskManager implements TaskManager {
     protected final Map<Integer, Subtask> subtasks = new HashMap<>();
     protected final HistoryManager historyManager = Managers.getDefaultHistory();
 
-    private int generateId() {
+    protected int generateId() {
         return nextId++;
     }
 
