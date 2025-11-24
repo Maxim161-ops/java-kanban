2c014485333ef7b9a92b72be0144a2df136a5e0e
Maxim161-ops <ddar2508-221B@yandex.ru>
2025-11-24 15:13:24 +0300

    Удалил импрот

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Map;
 
 public class Epic extends Task {
     private final List<Integer> subtaskIds = new ArrayList<>();
